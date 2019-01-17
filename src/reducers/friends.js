397552f86f5b041ff85a08843a486b7455fcf9e9
talexcrowell <talexcrowell@gmail.com>
2019-01-17 09:15:21 -0800

    refactored

--- src/reducers/friends.js
+++ src/reducers/friends.js
@@ -2,4 +2,35 @@ import {
   FETCH_USER_FRIENDS_REQUEST,
   FETCH_USER_FRIENDS_SUCCESS,
   FETCH_USER_FRIENDS_ERROR
-}from '../actions/users';
+}from '../actions/friends';
+
+const initialState ={
+  friendsList: null,
+  loading: false,
+  error: null
+};
+
+export default function reducer(state=intialState, action){
+  if(action.type === FETCH_USER_FRIENDS_REQUEST){
+    return{
+      ...state,
+      loading: true,
+      error: null
+    }
+  }
+  else if(action.type === FETCH_USER_FRIENDS_SUCCESS){
+    return{
+      friendsList: action.friends,
+      loading: false,
+      error: null
+    }
+  }
+  else if(action.type === FETCH_USER_FRIENDS_ERROR){
+    return{
+      ...state,
+      loading: true,
+      error: action.type
+    }
+  }
+  return state;
+}
