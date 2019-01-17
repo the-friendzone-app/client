70b396088b3305264dce2c7b82eeb875d691a587
ThomasChun <tchun2055@gmail.com>
2019-01-17 10:21:14 -1000

    added radio buttons and passed values to registerUser

--- src/components/registration-form.js
+++ src/components/registration-form.js
@@ -9,8 +9,10 @@ const matchesPassword = matches('password');
 
 export class RegistrationForm extends React.Component {
     onSubmit(values) {
-        const {username, password, firstName, lastName} = values;
-        const user = {username, password, firstName, lastName};
+        const {username, password, firstName, lastName, selfType, preferenceType } = values;
+        const user = {username, password, firstName, lastName, selfType, preferenceType};
+        console.log('self type:', selfType)
+        console.log('preference type:', preferenceType)
         return this.props
             .dispatch(registerUser(user))
             .then((res) => {
@@ -30,14 +32,25 @@ export class RegistrationForm extends React.Component {
                     this.onSubmit(values)
                 )}>
                 <label htmlFor="firstName">First name</label>
-                <Field component={Input} type="text" name="firstName" />
+                <Field 
+                    component={Input} 
+                    type="text" 
+                    name="firstName" 
+                    id="firstName" 
+                />
                 <label htmlFor="lastName">Last name</label>
-                <Field component={Input} type="text" name="lastName" />
+                <Field 
+                    component={Input} 
+                    type="text" 
+                    name="lastName" 
+                    id="lastName" 
+                />
                 <label htmlFor="username">Username</label>
                 <Field
                     component={Input}
                     type="text"
                     name="username"
+                    id="username"
                     validate={[required, nonEmpty, isTrimmed]}
                 />
                 <label htmlFor="password">Password</label>
@@ -45,6 +58,7 @@ export class RegistrationForm extends React.Component {
                     component={Input}
                     type="password"
                     name="password"
+                    id="password"
                     validate={[required, passwordLength, isTrimmed]}
                 />
                 <label htmlFor="passwordConfirm">Confirm password</label>
@@ -52,8 +66,21 @@ export class RegistrationForm extends React.Component {
                     component={Input}
                     type="password"
                     name="passwordConfirm"
+                    id="passwordConfirm"
                     validate={[required, nonEmpty, matchesPassword]}
                 />
+                <label htmlFor="selfType">Please identify the type of user you would like to be identified as:</label>
+                <div>
+                    <label><Field name="selfType" id="selfTypeTalker" component={Input} type="radio" value="talker"/> Talker</label>
+                    <label><Field name="selfType" id="selfTypeListener" component={Input} type="radio" value="listener"/> Listener</label>
+                    <label><Field name="selfType" id="selfTypeBoth" component={Input} type="radio" value="both"/> Both</label>
+                </div>
+                <label htmlFor="preferredType">Please identify that type of user you would prefer to interact with:</label>
+                <div>
+                    <label><Field name="preferenceType" id="preferenceTypeTalker" component={Input} type="radio" value="talker"/> Talker</label>
+                    <label><Field name="preferenceType" id="preferenceTypeListener" component={Input} type="radio" value="listener"/> Listener</label>
+                    <label><Field name="preferenceType" id="preferenceTypeBoth" component={Input} type="radio" value="both"/> Both</label>
+                </div>
                 <button
                     type="submit"
                     disabled={this.props.pristine || this.props.submitting}>
